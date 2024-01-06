d56bb416955bdf308ae34e5e224608f706afb6ba
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-06 11:37:30 +0530

    Create Area.java

---
+++ 9/Area.java
@@ -0,0 +1,17 @@
+/* Method overloading with type promotion. */
+
+package shubham;
+
+class Area {
+	void find(long l,long b) { 
+		System.out.println("Area is "+(l*b)) ; 
+	}
+	void find(int l, int b,int h) { 
+		System.out.println("Area is "+(l*b*h)); 
+	} 
+	public static void main (String[] args) { 
+		Area ar = new Area(); 
+		ar.find(8,5); //automatic type conversion from find(int,int) to find(long,long) . 
+		ar.find(2,4,6); //find(int l, int b,int h) is called. 
+	} 
+}
