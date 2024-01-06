77e2c21148338f9c6eb64c3daf3087e65b8a072b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-06 12:01:44 +0530

    Create Test.java

---
+++ 14/Test.java
@@ -0,0 +1,22 @@
+package shubham;
+
+public class Test {
+    static int x = 100;  // Static variable shared among all instances of the class
+    int y = 100;         // Instance variable specific to each instance of the class
+
+    public void increment() {
+        x++;  // Incrementing the static variable x
+        y++;  // Incrementing the instance variable y
+    }
+
+    public static void main(String[] args) {
+        Test t1 = new Test();  // Creating the first instance of the Test class
+        Test t2 = new Test();  // Creating the second instance of the Test class
+
+        t1.increment();  // Incrementing x and y for the first instance
+        t2.increment();  // Incrementing x and y for the second instance
+
+        System.out.println(t2.y);    // Printing the value of y for the second instance (t2)
+        System.out.println(Test.x);   // Printing the value of the static variable x (Test.x)
+    }
+}
