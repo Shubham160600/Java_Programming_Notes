d07135edb8a3bee21f51cb183b9fca9bda4b5ff5
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-06 12:01:56 +0530

    Create ST_Employee.java

---
+++ 13/ST_Employee.java
@@ -0,0 +1,25 @@
+package shubham;
+
+class ST_Employee {
+    int eid;
+    String name;
+    static String company_name = "Profound";
+
+    public void show() {
+        System.out.println(eid + " " + name + " " + company_name);
+    }
+
+    public static void main(String[] args) {
+        // Creating the first employee object
+        ST_Employee se1 = new ST_Employee();
+        se1.eid = 104;
+        se1.name = "Abhijit";
+        se1.show();
+
+        // Creating the second employee object
+        ST_Employee se2 = new ST_Employee();
+        se2.eid = 108;
+        se2.name = "Ankit";
+        se2.show();
+    }
+}
