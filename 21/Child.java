bfdf2bcfe2a0ae0d79140ff0de621ed13db73fa5
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-10 18:50:40 +0530

    Create Child.java

---
+++ 21/Child.java
@@ -0,0 +1,27 @@
+/* Example of Child class refering Parent class methods using super keyword. */
+
+package shubham;
+
+class Parent {
+    String name;
+
+    public void details() {
+        name = "Parent";
+        System.out.println(name);
+    }
+}
+
+public class Child extends Parent {
+    String name;
+
+    public void details() {
+        super.details(); // calling Parent class details() method
+        name = "Child";
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Child cobj = new Child();
+        cobj.details();
+    }
+}
