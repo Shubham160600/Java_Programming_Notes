dde5a5d2a972d1225488325ab67088e67e659eea
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 16:46:23 +0530

    Create Example.java

---
+++ 42/Example.java
@@ -0,0 +1,15 @@
+/* valueOf(). */
+
+package shubham;
+
+public class Example {
+    public static void main(String args[]) {
+        int num = 35;
+
+        // Converting int to String using String.valueOf()
+        String s1 = String.valueOf(num);
+
+        // Concatenating the converted string with another string
+        System.out.println(s1 + "IAmAString");
+    }
+}
