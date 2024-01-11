a7f52778285fc0891f1a748ca814b62c35d7864f
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:58:31 +0530

    Create Second.java

---
+++ 31/Second.java
@@ -0,0 +1,12 @@
+/* import all the classes from the particular package(Using packagename. */
+
+package Java;
+
+import LearnJava.*;
+
+class Second {
+    public static void main(String args[]) {
+        First obj = new First();
+        obj.msg();
+    }
+}
