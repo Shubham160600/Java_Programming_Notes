ea4b93f07800f839f96cc8375e87583acc689d77
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:49:06 +0530

    Create A.java

---
+++ 30/A.java
@@ -0,0 +1,9 @@
+/* import the only class you want to use(Using packagename.classname. */
+
+package pack;
+
+public class A {
+    public void msg() {
+        System.out.println("Hello");
+    }
+}
