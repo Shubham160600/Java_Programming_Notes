f3e9a463159094bd377504d9fd02bd9845944594
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:38:20 +0530

    Create B.java

---
+++ 29/B.java
@@ -0,0 +1,10 @@
+/* Using fully qualified name(But this is not a good practice. */
+
+package mypack;
+
+class B {
+public static void main(String args[]) {
+   pack.A obj = new pack.A(); // using fully qualified name
+   obj.msg();
+}
+}
