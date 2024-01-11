1e9c3187d0c7eed72468b24aa8126afb152d6677
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:58:40 +0530

    Create First.java

---
+++ 31/First.java
@@ -0,0 +1,9 @@
+/* import all the classes from the particular package(Using packagename. */
+
+package LearnJava;
+
+public class First {
+    public void msg() {
+        System.out.println("Hello");
+    }
+}
