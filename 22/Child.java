8531084be8d60503c214498a982eb49334bea4b2
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:20:58 +0530

    Create Child.java

---
+++ 22/Child.java
@@ -0,0 +1,29 @@
+/* Example of Child class calling Parent class constructor using super keyword. */
+
+package shubham;
+
+class Parent {
+    String name;
+
+    public Parent(String n) {
+        name = n;
+    }
+}
+
+public class Child extends Parent {
+    String name;
+
+    public Child(String n1, String n2) {
+        super(n1);  // passing argument to the parent class constructor
+        this.name = n2;
+    }
+
+    public void details() {
+        System.out.println(super.name + " and " + name);
+    }
+
+    public static void main(String[] args) {
+        Child cobj = new Child("Parent", "Child");
+        cobj.details();
+    }
+}
