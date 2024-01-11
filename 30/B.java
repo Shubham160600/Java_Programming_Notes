eac93ea7fb9ed1001b811942baf1090efd593762
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-11 11:48:53 +0530

    Create B.java

---
+++ 30/B.java
@@ -0,0 +1,12 @@
+/* import the only class you want to use(Using packagename.classname. */
+
+package mypack;
+
+import pack.A;
+
+class B {
+    public static void main(String args[]) {
+        A obj = new A();
+        obj.msg();
+    }
+}
