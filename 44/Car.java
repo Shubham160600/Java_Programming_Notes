dea1d8d36a19ce785c8023510390338989cd0bb5
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-12 17:18:07 +0530

    Create Car.java

---
+++ 44/Car.java
@@ -0,0 +1,10 @@
+/* toString(). */
+
+package shubham;
+
+public class Car {
+    public static void main(String args[]) {
+        Car c = new Car(); // Creating an instance of the Car class
+        System.out.println(c); // Printing the default string representation of the Car object
+    }
+}
