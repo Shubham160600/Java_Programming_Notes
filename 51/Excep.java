993738bc258ebcee0cb4e26ec69cf45b420b7386
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-22 16:36:38 +0530

    Create Excep.java

---
+++ 51/Excep.java
@@ -0,0 +1,21 @@
+/*Nested try statement.*/  
+
+package shubham;
+ 
+class Excep {
+    public static void main(String[] args) {
+        try {
+            int arr[] = {5, 0, 1, 2};
+            
+            try {
+                int x = arr[3] / arr[1];
+            } catch (ArithmeticException ae) {
+                System.out.println("divide by zero");
+            }
+
+            arr[4] = 3; // This line will throw ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("array index out of bound exception");
+        }
+    }
+}
